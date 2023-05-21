de86f48ded04bd336276d2fa0799565f0ad40fd7
volvojakobsen <91479420+volvojakobsen@users.noreply.github.com>
2023-05-22 01:38:56 +0200

    working on calendar

--- holidaze/src/pages/venueInfo/venueInfo.jsx
+++ holidaze/src/pages/venueInfo/venueInfo.jsx
@@ -1,6 +1,13 @@
 import React, {useState, useEffect, useContext} from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import { differenceInCalendarDays, getDate, isWithinInterval, toDate, format } from 'date-fns';
+import { DayPicker } from 'react-day-picker';
+import 'react-day-picker/dist/style.css';
+
+
+
+
 
 const Content = styled.div`
 display: flex;
@@ -21,6 +28,25 @@ export const VenueInfo = () => {
    const [singleVenue, setSingleVenue] = useState([]);
    const [isLoading, setIsLoading] = useState(false);
    const [isError, setIsError] = useState(false);
+   const [date, setDate] = useState(new Date());
+   const [disabledStartDates, setDisabledStartDates] = useState([]);
+   const [disabledEndDates, setDisabledEndDates] = useState([]);
+   const [disabledDates, setDisabledDates] = useState([]);
+   const [bookings, setBookings] = useState([]);
+   const [value, setValue] = useState(new Date());
+   const [selectedDay, setSelectedDay] = useState(null);
+
+   const disabledDays = [
+    new Date(2023, 5, 22),
+    new Date(2023, 0, 25),
+    new Date(2023, 5, 1),
+    { from: new Date(2022, 4, 18), to: new Date(2022, 4, 29) }
+  ];
+
+   const onChange = date => {
+    setDate(date)
+   }
+   //console.log(date)
   
  
    useEffect(() => {
@@ -31,6 +57,7 @@ export const VenueInfo = () => {
          const response = await fetch(`https://api.noroff.dev/api/v1/holidaze/venues/${id.id}?_bookings=true`);
          const json = await response.json();
          setSingleVenue(json);
+         setBookings(json.bookings)
          setIsLoading(false);
        } catch (error) {
          setIsLoading(false);
@@ -42,7 +69,45 @@ export const VenueInfo = () => {
    }, [id.id]);
 
    
+
    
+
+   
+
+   for(let i = 0; i < bookings.length; i++) {
+   const from = `${bookings[i].dateFrom}`;
+   const to = `${bookings[i].dateTo}`;
+   const dateRanges = [from, to];
+   const disabledDayFrom = {
+    year: from.substring(0,4),
+    month: from.substring(5, 7),
+    day: from.substring(8, 10),
+  }
+  const disabledDayTo = {
+    year: to.substring(0,4),
+    month: to.substring(5, 7),
+    day: to.substring(8, 10),
+  }
+  disabledDates.push(from);
+  //console.log(disabledDayFrom);
+  //console.log(from);
+ 
+};
+
+const [selected, setSelected] = useState(new Date());
+
+console.log(selected);
+
+let footer = <p>Please pick a day.</p>;
+if (selected) {
+  footer = <p>You picked {format(selected, 'PP')}.</p>;
+}
+
+const tull = "2023-05-24";
+
+//JSON.stringify(disabledDays);
+console.log(disabledDays)
+
  
    if (isLoading) {
      return <div className="loader"></div>;
@@ -63,6 +128,12 @@ export const VenueInfo = () => {
         <h2>Price: {singleVenue.price}</h2>
         <p>max guests: {singleVenue.maxGuests}</p>
       </Row>
+      <DayPicker mode="single"
+      selected={selected}
+      onSelect={setSelected}
+      disabled={disabledDays}
+      footer={footer} />;
+      <p>{date.toDateString()}</p>
       <p>{singleVenue.description}</p>
     </div>
    </Content>
